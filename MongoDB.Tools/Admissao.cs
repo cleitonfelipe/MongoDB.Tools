984862ccc48eb715f5bed29e23d26d30fdd6c977
Cleiton Felipe de Moraes <cleiton.felipe@yahoo.com.br>
2021-04-15 09:08:17 -0300

    Add project

---
+++ MongoDB.Tools/Admissao.cs
@@ -0,0 +1,158 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MongoDB.Tools
+{
+    public class Admissao
+    {
+        [BsonId()]
+        public ObjectId Id { get; set; }
+
+        [BsonElement("coop_location")]
+        public string coop_location { get; set; }
+
+        [BsonElement("cpf")]
+        public string cpf { get; set; }
+
+        [BsonElement("phone")]
+        public string phone { get; set; }
+
+        [BsonElement("social_name")]
+        public string social_name { get; set; }
+
+        [BsonElement("birth_date")]
+        public string birth_date { get; set; }
+
+        [BsonElement("full_name")]
+        public string full_name { get; set; }
+
+        [BsonElement("document_kind")]
+        public string document_kind { get; set; }
+
+        [BsonElement("document_number'")]
+        public string document_number { get; set; }
+
+        [BsonElement("document_issue_date")]
+        public string document_issue_date { get; set; }
+
+        [BsonElement("document_issue_entity")]
+        public string document_issue_entity { get; set; }
+
+        [BsonElement("document_issue_uf")]
+        public string document_issue_uf { get; set; }
+
+        [BsonElement("citizenship")]
+        public string citizenship { get; set; }
+
+        [BsonElement("birth_city")]
+        public string birth_city { get; set; }
+
+        [BsonElement("birth_uf ")]
+        public string birth_uf { get; set; }
+
+        [BsonElement("mother_name")]
+        public string mother_name { get; set; }
+
+        [BsonElement("address_cep")]
+        public string address_cep { get; set; }
+
+        [BsonElement("address_street")]
+        public string address_street { get; set; }
+
+        [BsonElement("address_number")]
+        public string address_number { get; set; }
+
+        [BsonElement("address_complement")]
+        public string address_complement { get; set; }
+
+        [BsonElement("address_district")]
+        public string address_district { get; set; }
+
+        [BsonElement("address_city")]
+        public string address_city { get; set; }
+
+        [BsonElement("address_uf")]
+        public string address_uf { get; set; }
+
+        [BsonElement("email")]
+        public string email { get; set; }
+
+        [BsonElement("income")]
+        public string income { get; set; }
+
+        [BsonElement("terms")]
+        public bool terms { get; set; }
+
+        [BsonElement("left_selfie")]
+        public string left_selfie { get; set; }
+
+        [BsonElement("right_selfie")]
+        public string right_selfie { get; set; }
+
+        [BsonElement("front_selfie")]
+        public string front_selfie { get; set; }
+
+        [BsonElement("document_front")]
+        public string document_front { get; set; }
+
+        [BsonElement("document_back")]
+        public string document_back { get; set; }
+
+        [BsonElement("address_proof")]
+        public string address_proof { get; set; }
+
+        [BsonElement("interests")]
+        public string interests { get; set; }
+
+        [BsonElement("status_code")]
+        public string status_code { get; set; }
+
+        [BsonElement("status")]
+        public string status { get; set; }
+
+        [BsonElement("description")]
+        public string description { get; set; }
+
+        [BsonElement("process_number")]
+        public string process_number { get; set; }
+
+        [BsonElement("sms_code")]
+        public int sms_code { get; set; }
+
+        [BsonElement("sms_valid")]
+        public bool sms_valid { get; set; }
+
+        [BsonElement("cooperative")]
+        public string cooperative { get; set; }
+
+        [BsonElement("agency")]
+        public string agency { get; set; }
+
+        [BsonElement("account")]
+        public string account { get; set; }
+
+        [BsonElement("date_creation")]
+        public string date_creation { get; set; }
+
+        [BsonElement("last_user")]
+        public string last_user { get; set; }
+
+        [BsonElement("comments")]
+        public List<Comments> comments { get; set; }
+    }
+
+    public class Comments
+    {
+        [BsonElement("user")]
+        public string user { get; set; }
+
+        [BsonElement("date_comments")]
+        public string date_comments { get; set; }
+
+        [BsonElement("comments")]
+        public string comments { get; set; }
+    }
+}
---
+++ MongoDB.Tools/Program.cs
@@ -0,0 +1,42 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.IO;
+using MongoDB.Bson.Serialization;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Authentication;
+
+namespace MongoDB.Tools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = $@"mongodb://poc-cosmo-db:lujwqRyLNdWbpkxRaMy8Co6nwWIB8M3bTkisxJgdGyh5q6eEh9BfGbgDX4rMQex2xNaIZhciWl4fFVZNV2BPTw==@poc-cosmo-db.documents.azure.com:10255/?ssl=true&replicaSet=globaldb";
+            MongoClientSettings settings = MongoClientSettings.FromUrl(new MongoUrl(connectionString));
+            settings.SslSettings = new SslSettings() { EnabledSslProtocols = SslProtocols.Tls12 };
+            var client = new MongoClient(settings);
+
+            //BsonClassMap.RegisterClassMap<Admissao>(cm => {
+            //    cm.AutoMap();
+            //    cm.GetMemberMap(c => c.comments).;
+            //});
+
+            var database = client.GetDatabase("AdmissaoDigital");
+            var Collection = database.GetCollection<Admissao>("ContratosAdmissao");
+
+            var dados = Collection.Find(new BsonDocument()).ToList();
+
+            //StreamWriter streamWriter = new StreamWriter(@"C:\Users\omehe\Desktop\FileMongo\scriptBackup.json");
+
+            //foreach (var item in dados)
+            //{
+            //    streamWriter.WriteLine(item.ToJson());
+            //}
+
+            //streamWriter.Close();
+        }
+    }
+}
