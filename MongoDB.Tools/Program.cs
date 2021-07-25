c1b80392e3353a76ea2a64c51b8fa6314a033bee
Cleiton Felipe de Moraes <omehe.cleiton@hotmail.com>
2021-07-25 14:04:10 -0300

    Update

--- MongoDB.Tools/Program.cs
+++ MongoDB.Tools/Program.cs
@@ -11,49 +11,65 @@ namespace MongoDB.Tools
     {
         static void Main(string[] args)
         {
-            string connectionString = $@"<connectionstring>";
-            string pathToCAFile = @"<path/certicate file>";
+            string connectionString = @"mongodb://localhost:27018";
+            //string pathToCAFile = @"<path/certicate file>";
 
-            X509Store localTrustStore = new X509Store(StoreName.Root);
-            X509Certificate2Collection certificateCollection = new X509Certificate2Collection();
+            //X509Store localTrustStore = new X509Store(StoreName.Root);
+            //X509Certificate2Collection certificateCollection = new X509Certificate2Collection();
             //certificateCollection.Import(pathToCAFile);
-            certificateCollection.ImportFromPem(pathToCAFile);
+            //certificateCollection.ImportFromPem(pathToCAFile);
             //var cert = new X509Certificate2(pathToCAFile);
 
+            //try
+            //{
+            //    localTrustStore.Open(OpenFlags.ReadWrite);
+            //    localTrustStore.AddRange(certificateCollection);
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine("Root certificate import failed: " + ex.Message);
+            //    throw;
+            //}
+            //finally
+            //{
+            //    localTrustStore.Close();
+            //}
+
+            //MongoClientSettings settings = MongoClientSettings.FromUrl(new MongoUrl(connectionString));
+            //settings.SslSettings = new SslSettings
+            //{
+            //    EnabledSslProtocols = SslProtocols.Tls12,
+            //    ServerCertificateValidationCallback = delegate (object s, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+            //    {
+            //        return certificate.GetCertHashString() == "<Hexadecimal Certificate hash value>";
+            //    }
+            //};
+            var client = new MongoClient(connectionString);
+
+            var database = client.GetDatabase("MongoDBTools");
+            var Collection = database.GetCollection<Model>("blogPost");
+
             try
             {
-                localTrustStore.Open(OpenFlags.ReadWrite);
-                localTrustStore.AddRange(certificateCollection);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Root certificate import failed: " + ex.Message);
-                throw;
-            }
-            finally
-            {
-                localTrustStore.Close();
-            }
+                var comando = new BsonDocument("ping", 1);
+                var testConnection = database.RunCommandAsync<BsonDocument>(comando).Result;
 
-            MongoClientSettings settings = MongoClientSettings.FromUrl(new MongoUrl(connectionString));
-            settings.SslSettings = new SslSettings
-            {
-                EnabledSslProtocols = SslProtocols.Tls12,
-                ServerCertificateValidationCallback = delegate (object s, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+                Console.WriteLine($"{testConnection}");
+
+                Console.WriteLine("Inserting data...");
+
+                var model = new Model 
                 {
-                    return certificate.GetCertHashString() == "<Hexadecimal Certificate hash value>";
-                }
-            };
-            var client = new MongoClient(settings);
+                    Title = "Frist Post",
+                    Description = "This is a frist Post",
+                    Post = "Frist Post from my blog."
+                };
 
-            var database = client.GetDatabase("<DatabaseName>");
-            var Collection = database.GetCollection<Model>("ColletionName");
+                Collection.InsertOne(model);
 
-            try
-            {
-                var dados = Collection.Find(new BsonDocument()).CountDocuments();
-
-                Console.WriteLine($"{dados}");
+                var count = Collection.Find(new BsonDocument()).CountDocuments();
+                var data = Collection.Find(new BsonDocument()).FirstOrDefault();
+                Console.WriteLine($"{data.Id} | {data.Title} | {data.Description} | {data.Post}");
             }
             catch (Exception ex)
             {
